9305e5d5dd31f8af679ad905adca576e9f2b3e73
uncooldude <armedwarri.or.t.s@gmail.com>
2024-12-10 22:07:08 +0530

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -70,7 +70,7 @@ const Sidebar = ({ isOpen, onClose }) => {
               >
                 <FontAwesomeIcon
                   icon={item.icon}
-                  className="text-[#ffbade] text-xl font-bold max-[575px]:text-[15px]"
+                  className="text-[#3324f2] text-xl font-bold max-[575px]:text-[15px]"
                 />
                 <p className="text-[15px] max-[575px]:text-[13px]">
                   {item.label}
@@ -87,7 +87,7 @@ const Sidebar = ({ isOpen, onClose }) => {
                       index === 0 ? "rounded-l-[3px]" : "rounded-r-[3px]"
                     } ${
                       language === lang
-                        ? "bg-[#ffbade] text-black"
+                        ? "bg-[#3324f2] text-black"
                         : "bg-gray-600 text-white"
                     } max-[575px]:text-[9px] max-[575px]:py-0`}
                   >
@@ -121,7 +121,7 @@ const Sidebar = ({ isOpen, onClose }) => {
               >
                 <Link
                   to={item.path}
-                  className="px-4 hover:text-[#ffbade] hover:cursor-pointer w-fit line-clamp-1"
+                  className="px-4 hover:text-[#3324f2] hover:cursor-pointer w-fit line-clamp-1"
                 >
                   {item.name}
                 </Link>
